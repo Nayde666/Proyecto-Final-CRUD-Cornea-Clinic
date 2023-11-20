61ef3c8093fc640c89a0dfb43671653265c0c56c
CastroPerla <perlacass936@gmail.com>
2023-11-20 09:41:35 -0600

    Creacion de rama

--- Frontend/components/Tutorial.vue
+++ Frontend/components/Tutorial.vue
@@ -1,4 +1,4 @@
-<!-- Please remove this file from your project -->
+<!-- Please remove this file your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
