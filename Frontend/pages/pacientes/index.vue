5f2ebd0a9b400b4539fe20b4b98804cbc09fb04a
Orlando Javier <oj.flores118@gmail.com>
2023-11-22 14:20:11 -0600

    Visualizar paginas Perla

---
+++ Frontend/pages/pacientes/index.vue
@@ -0,0 +1,15 @@
+<script>
+import patRegister from '~/components/Users/patRegister.vue';
+import PatRegister from '../../components/Users/patRegister.vue';
+export default {
+    layout: 'pacientes',
+    components: {
+        patRegister
+       
+    }
+}
+</script>
+
+<template>
+    <pat-register />
+</template>
