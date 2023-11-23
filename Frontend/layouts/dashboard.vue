591a241dae3776f86a0101eb24e6d81a8cd528cc
Orlando Javier <oj.flores118@gmail.com>
2023-11-23 09:28:33 -0600

    dashboard

--- Frontend/layouts/dashboard.vue
+++ Frontend/layouts/dashboard.vue
@@ -1,8 +1,23 @@
 <script>
+   import draweVue from '~/components/Dashboard/drawe.vue';
+   export default {
+    components: {
+        draweVue
+    }, data () {
+        return {
 
+        }
+    }
+}
 </script>
 
 <template>
+    <v-app>
+        <draweVue/>
+            <v-main>
+                <Nuxt />
+            </v-main> 
+    </v-app>
 </template>
 
 <style scoped>
